597cbcc50a0c100edcb8a2568b15cb55ae1a048c
Toasty <160161673+HYToasty@users.noreply.github.com>
2025-10-24 01:08:27 +0200

    Delete Assets directory

--- Assets/ScriptableObjects/Vehicles/Vehicles.cs
+++
@@ -1,9 +0,0 @@
-using UnityEditor;
-using UnityEngine;
-using UnityEngine.Rendering;
-
-[CreateAssetMenu(fileName = "Blank Vehicle", menuName = "Instances/Vehicle")]
-public class Vehicles : ScriptableObject
-{
-    public GameObject vehicle;
-}
